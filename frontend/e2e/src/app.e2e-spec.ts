41eed7ad2ac462eedc0418945deab3df85f06ed8
stalinladino <eladino@todo1.com>
2021-06-15 21:20:09 -0500

    DockerFile

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('fronend-services app is running!');
+    expect(page.getTitleText()).toEqual('frontend-aks app is running!');
   });
 
   afterEach(async () => {
--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fronend-services'`, () => {
+  it(`should have as title 'frontend-aks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fronend-services');
+    expect(app.title).toEqual('frontend-aks');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fronend-services app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('frontend-aks app is running!');
   });
 });
